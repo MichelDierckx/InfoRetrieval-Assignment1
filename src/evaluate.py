fix recall and precision

src/evaluate.py
@@ -21,8 +21,14 @@ def evaluate(result_file: str, expected_result_file: str, k: int):
 def evaluate_precision(result_dict: dict, expected_dict: dict, k: int):
     precision_list = []
     for key in result_dict.keys():
-        relevant_docs = get_amount_of_relevant_docs(result_dict, expected_dict, key, k)
-        precision_list.append(relevant_docs /min(k, len(result_dict[key])))
+        relevant = []
+        if key in expected_dict.keys():
+            for i in range(1, min(k, len(expected_dict[key])) + 1):
+                relevant_docs = get_amount_of_relevant_docs(result_dict, expected_dict, key, i)
+                relevant.append(relevant_docs/i)
+            precision_list.append(np.mean(relevant))
+        else:
+            precision_list.append(0)
     if len(precision_list) != 0:
         result = np.mean(precision_list)
         print(f'Mean average precision at {k}: {result}')
@@ -31,8 +37,11 @@ def evaluate_precision(result_dict: dict, expected_dict: dict, k: int):
 def evaluate_recall(result_dict: dict, expected_dict: dict, k: int):
     recall_list = []
     for key in result_dict.keys():
-        relevant_docs = get_amount_of_relevant_docs(result_dict, expected_dict, key, k)
-        recall_list.append(relevant_docs /min(k, len(expected_dict[key])))
+        if key in expected_dict.keys():
+            relevant_docs = get_amount_of_relevant_docs(result_dict, expected_dict, key, k)
+            recall_list.append(relevant_docs/len(expected_dict[key]))
+        else:
+            recall_list.append(0)
     if len(recall_list) != 0:
         result = np.mean(recall_list)
         print(f'Mean average recall at {k}: {result}')
@@ -50,5 +59,5 @@ def get_amount_of_relevant_docs(result_dict: dict, expected_dict: dict, key: str
 
 if __name__ == "__main__":
     path = "results/rankings/"
-    evaluate(path + "dev_query_results.csv", path + "dev_queries_ranking.csv", 10)
+    evaluate(path + "dev_queries_ranking.csv", path + "dev_query_results.csv", 5)
 