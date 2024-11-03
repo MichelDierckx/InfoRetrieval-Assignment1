cc821cfb43ebaf200a830aa30f4ea9c0c650dd34
benjaminpowell <benjamin.powell@student.uantwerpen.be>
2024-11-03 11:25:49 +0000

    adding the evaluate function to main and add the expected results to the rankings directory

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pstats
 
 from inverted_index import Indexer, DocumentRanker, InvertedIndex
 from tokenizer import Tokenizer
+from evaluate import evaluate
 
 # Document collections
 FULL_DOCS_SMALL = "full_docs_small"
@@ -39,6 +40,10 @@ DEV_QUERIES_RANKING = "dev_queries_ranking.csv"
 DEV_QUERIES_SMALL_RANKING = "dev_queries_small_ranking.csv"
 QUERIES_RANKING = "queries_rankings.csv"
 
+# Expected rankings filenames
+DEV_QUERIES_EXPECTED_RESULT = "dev_query_results.csv"
+DEV_QUERIES_EXPECTED_RESULT_SMALL = "dev_query_results_small.csv"
+
 # Flags
 BATCH_SIZE = 2000
 SAVE_TOKENIZED_DOCUMENTS = False
@@ -64,6 +69,7 @@ def main():
             queries_file = f'{DIRECTORY_QUERIES}/{DEV_QUERIES_SMALL[0]}'
             queries_file_delimiter = DEV_QUERIES_SMALL[1]
             queries_ranking = f'{DIRECTORY_RANKINGS}/{DEV_QUERIES_SMALL_RANKING}'
+            queries_expected = f'{DIRECTORY_RANKINGS}/{DEV_QUERIES_EXPECTED_RESULT_SMALL}'
         elif SIZE == "FULL":
             documents_dir = f'{DIRECTORY_DOCUMENTS}/{FULL_DOCS}'
             index_file = f'{DIRECTORY_INDEX}/{FULL_DOCS}/{DEFAULT_INDEX_FILE_NAME}'
@@ -74,6 +80,7 @@ def main():
             queries_file = f'{DIRECTORY_QUERIES}/{DEV_QUERIES[0]}'
             queries_file_delimiter = DEV_QUERIES[1]
             queries_ranking = f'{DIRECTORY_RANKINGS}/{DEV_QUERIES_RANKING}'
+            queries_expected = f'{DIRECTORY_RANKINGS}/{DEV_QUERIES_EXPECTED_RESULT}'
         else:
             print(f'Size {SIZE} should be SMALL or FULL')
             return
@@ -122,7 +129,8 @@ def main():
 
     document_ranker.rank_queries_from_file(input_file=queries_file, output_file=queries_ranking,
                                            delimiter=queries_file_delimiter, top_k=k)
-
+    if DEV:
+        evaluate(queries_ranking, queries_expected, k)
     # Stop profiling
     profiler.disable()
 
